37c240da2eb54391aab13becfa0a16ed84666a53
ceb3745 <ceb3745@g.rit.edu>
2019-03-20 19:51:10 -0400

    formatted stuff

--- src/Main.java
+++ src/Main.java
@@ -43,7 +43,7 @@ public class Main {
         /**
          * Start the select query
          */
-        sb.append("SELECT ");
+        //sb.append("SELECT ");
 
         /**
          * If we gave no columns just give them all to us
@@ -51,17 +51,23 @@ public class Main {
          * other wise add the columns to the query
          * adding a comma top seperate
          */
-        sb.append("* ");
+        //sb.append("* ");
 
         /**
          * Tells it which table to get the data from
          */
-        sb.append("FROM PAPER ");
+        //sb.append("FROM PAPER ");
 
         /**
          * close with semi-colon
          */
-        sb.append(";");
+        //sb.append(";");
+
+        sb.append("with storeSalesProduct (product_name,UPC,quantity, storeID) as \n" +
+                "(select product_name,product.UPC, saleitem.quantity, sale.store_id from\n" +
+                "product natural join saleitem natural join sale where sale.store_id=1075)\n" +
+                "select UPC,Product_name, storeID,sum(quantity) as total_quantity from storeSalesProduct group by UPC order by total_quantity limit 20;\n");
+
 
         //Print it out to verify it made it right
         System.out.println("Query: " + sb.toString());
@@ -70,7 +76,14 @@ public class Main {
              * Execute the query and return the result set
              */
             Statement stmt = demo.getConnection().createStatement();
-            System.out.println(stmt.executeQuery(sb.toString()));
+            ResultSet result = stmt.executeQuery(sb.toString());
+
+            while(result.next()){
+                System.out.println("UPC: " + result.getString(1)
+                                    + "\nName: " + result.getString(2)
+                                    + "\nStore Number: " + result.getString(3)
+                                    + "\nQuantity: " + result.getString(4) );
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }

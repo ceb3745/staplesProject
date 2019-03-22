6b5938bfdb3d252f7d1a6515388c81bcba80d242
Jason Ziskind <jmz1040@cs.rit.edu>
2019-03-22 16:44:06 -0400

    added the case in the switch to acces the DBAdmin

--- src/MainPk/Main.java
+++ src/MainPk/Main.java
@@ -1,5 +1,6 @@
 package MainPk;
 
+import DBAdmin.DBAdminController;
 import POS_Application.ClientControl;
 import POS_Application.POS_UI;
 
@@ -30,7 +31,8 @@ public class Main {
 
         switch (choice) {
             case 1:
-                // Run Admin UI
+                DBAdminController DBC = new DBAdminController();
+                DBC.startUp();
                 break;
             case 2:
                 // Run Cashier UI

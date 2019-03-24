bf92f7a919d1ba5de35af94c263b742f538e9df7
Jason Ziskind <jmz1040@cs.rit.edu>
2019-03-24 19:34:12 -0400

    quick changes to DBAview

--- src/DBAdmin/DBAdminView.java
+++ src/DBAdmin/DBAdminView.java
@@ -25,7 +25,7 @@ public class DBAdminView {
         System.out.println("Entering security protocol...\n");
         //security check
         Security mySecurity = new Security(userPass);
-        int check = 0;//mySecurity.securityCheckpoint();
+        int check = mySecurity.securityCheckpoint();
         if(check == 1){
             System.out.println("Exiting database...");
         }else{
@@ -47,10 +47,20 @@ public class DBAdminView {
                 if(input.equalsIgnoreCase("Quit")){
                     done = true;
                 }else if (input.equalsIgnoreCase("Help")) {
-                    System.out.println("helpfull message");
+                    System.out.println("-Make sure your SQL statements have no line breaks.\n");
+                    System.out.println("-Type \"Quit\" to close this view and the database.\n");
+                    System.out.println("-To get the top 20 selling products at a particular store,\n" +
+                            "Try the following statement, which queries store 1075:\n" +
+                            "\"with storeSalesProduct (product_name,UPC,quantity, storeID) as (select product_name,product.UPC, saleitem.sale_quantity, sale.store_id from product natural join saleitem natural join sale where sale.store_id=1075) select UPC,Product_name, storeID,sum(quantity) as total_quantity from storeSalesProduct group by UPC order by total_quantity desc limit 20;\"\n");
+                    System.out.println("To get the top 20 selling products in a particular state,\n" +
+                            "Try the followinf statement, which queries New York State:\n" +
+                            "\"with storeSalesProduct (product_name,UPC,quantity, storeID,state) as (select product_name,product.UPC, saleitem.sale_quantity, sale.store_id, state from product natural join saleitem natural join sale natural join store where state = 'NY') select UPC,Product_name, storeID,sum(quantity) as total_quantity, state from storeSalesProduct group by UPC order by total_quantity desc limit 20;\"\n");
+                    System.out.println("To get the top 5 stores by sales, Try the following statement:\n" +
+                            "\"with A (product_name,UPC,quantity, storeID,price,Sale_ID,year) as (select product_name,product.UPC, sale_quantity, sale.store_id,price, sale.Sale_ID,year from product natural join saleitem natural join sale where year = 2019) select storeID,sum(quantity*price) as sales from A group by storeID order by sales desc limit 5;\"\n");
+
                 }else{
                     //send input to controller
-                    System.out.println("sending input \n\"" + input + "\"\nto the database...");
+                    System.out.println("Sending input \n\"" + input + "\"\nto the database...");
                     try {
                         ResultSet result = myDBC.getResult(input);
                         if (result != null){
